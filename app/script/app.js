b88d6aa44d9285b2c64a517aff8e91322b402c74
guinovi <129969998+guinovi@users.noreply.github.com>
2024-04-28 18:11:25 -0300

    form dynamic js 1

--- app/script/app.js
+++ app/script/app.js
@@ -1,23 +1,20 @@
 /* DECLARACIONES */
+//Burger
 const burger = document.getElementById("burger");
 const navHeader = document.getElementById('navHeader')
 const subrayado = document.querySelectorAll(".header-li");
+//FORM COTIZACION
+const btnNext = document.getElementById('btnNext');
+const btnSend = document.getElementById('btnSend');
+const form1 = document.getElementById('form1');
+const form2 = document.getElementById('form2');
+const form3 = document.getElementById('form3');
 
 
 
 
 
-/* burger */
-/* burger.addEventListener('click',()=>{
-    // console.log(navHeader.style.display);
-    if (navHeader.style.display === "none") {
-        // subrayado.forEach((e)=>console.log(e))
-        subrayado.forEach((e)=> e.classList.remove("subrayado"));
-        navHeader.style.display = "block"
-    } else {
-        navHeader.style.display = "none"
-    }
-}) */
+/* BURGER INDEX */
 let menuAbierto = false;
 burger.addEventListener('click', () => {
     if (!menuAbierto) {
@@ -30,3 +27,15 @@ burger.addEventListener('click', () => {
         menuAbierto = false;
     }
 });
+
+/* FORM  COTIZACION*/
+btnNext.addEventListener('click', ()=>{
+    
+    btnSend.classList.toggle('display-none')
+    const formA = Array.from(form1.children)
+    formA.forEach((e)=>{
+        e.classList.toggle('display-none')
+    })
+})
+
+
---
+++ form.js
@@ -0,0 +1,36 @@
+nombre
+email
+tel
+
+
+2: Donde?
+Interior
+Exterior
+Ambas
+
+3:
+Hogar
+Establecimiento / Comercio
+
+4:
+Departamento
+Casa
+
+5: Cantidad de ambientes donde desea colocar 
+dispositivos de seguridad / video vgilancia
+
+6: De los habientes antes mecionados cuntos tienen 
+salidas al exterior? (INCLUIDO Jardin / terrazas)
+
+
+
+siguiente
+desactiva btns 
+hasta el final
+
+
+
+
+
+
+
